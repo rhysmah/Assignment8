aa4857acf1734517fd324ae2db19f8349bcf9f50
rhysmah <rhys.mahannah@gmail.com>
2023-03-25 16:39:12 -0700

    Updated create.c

--- create.c
+++ create.c
@@ -1,121 +1,93 @@
-////
-//// Created by Rhys Mahannah on 2023-03-18.
-////
 //
-//#include <stdio.h>
-//#include <stdarg.h>
-//#include <string.h>
-//#include <stdlib.h>
+// Created by Rhys Mahannah on 2023-03-18.
 //
-//#define MAX_CHAR_LENGTH  20
-//#define INPUT_FILE_NAME  "accounts.csv"
-//#define OUTPUT_FILE_NAME "accounts.dat"
-//
-//typedef struct {
-//    int    AccountNumber;
-//    char   FirstName[MAX_CHAR_LENGTH];
-//    char   LastName[MAX_CHAR_LENGTH];
-//    double AccountBalance;
-//    double LastPaymentAmount;
-//} CustomerData;
-//
-//// FUNCTION PROTOTYPES
-//void readWriteCustomerDataToBinaryFile(int numOfArgs, const char inputFileName[], const char outputFileName[], ...);
-//
-//// DRIVES THE PROGRAM
-//int main() {
-//    readWriteCustomerDataToBinaryFile(2, INPUT_FILE_NAME, OUTPUT_FILE_NAME);
-//}
-//
-//// FUNCTION DEFINITIONS
-//void readWriteCustomerDataToBinaryFile(int numOfArgs, const char inputFileName[], const char outputFileName[], ...) {
-//
-//    // CONFIRM ARGUMENT COUNT
-//    int count;
-//    int total = 0;
-//    va_list args;
-//
-//    va_start(args, outputFileName);
-//    for (count = 0; count < numOfArgs; ++count) {
-//        total += va_arg(args, int);
-//    }
-//    va_end(args);
-//
-//    if (count != 2) {
-//        puts("\nYou must input two arguments for this function:\n"
-//             "(1) An input file name.\n"
-//             "(2) the name of the output file.\n"
-//             "Program terminating.\n");
-//        exit(EXIT_FAILURE);
-//    }
-//
-//    // RUN FUNCTION
-//    FILE *inputCSVFilePtr;  // Pointer to CSV file to be read.
-//    FILE *outputDATFilePtr; // Pointer to DAT file to be created.
-//
-//    // (1) Open CSV input file in read-only mode.
-//    if ((inputCSVFilePtr = fopen(inputFileName, "rb")) == NULL) {
-//        printf("\nERROR: \"%s\" not found. Terminating operation.", inputFileName);
-//        exit(EXIT_FAILURE);
-//    }
-//
-//    // (2) Check if DAT output file already exists.
-//    if ((outputDATFilePtr = fopen(outputFileName, "rb")) != NULL) {
-//
-//        // DAT file confirmed to exist and is currently opened in read-only mode.
-//        // Immediately close file; if user wants to overwrite current file, it will be re-opened in write mode.
-//        fclose(outputDATFilePtr);
-//
-//        // (3) If DAT file already exists, ask user if they want to overwrite it.
-//        char userChoice;
-//        printf("\n"
-//               "File \"%s\" already exists. Overwrite?\n"
-//               "y or n: ", outputFileName);
-//        scanf("%c", &userChoice);
-//
-//        while (userChoice != 'y' && userChoice != 'n') {
-//            puts("Please enter 'y' for yes or 'n' for no.\n");
-//            scanf("%c", &userChoice);
-//        }
-//
-//        if (userChoice == 'n') {
-//            printf("\n"
-//                   "File \"%s\" will not be overwritten. Terminating program.\n", outputFileName);
-//            exit(EXIT_SUCCESS);
-//        }
-//    }
-//
-//    // (4) Open DAT file in write mode.
-//    if ((outputDATFilePtr = fopen(outputFileName, "wb")) == NULL) {
-//        printf("\nERROR: \"%s\" not accessed. Terminating operation.", outputFileName);
-//        exit(EXIT_FAILURE);
-//    }
-//
-//    // (5) Write contents from CSV file into DAT file (binary).
-//    CustomerData tempCustomer = {0, "", "", 0.0, 0.0}; // Dummy data
-//
-//    printf("\nWriting data from \"%s\" to \"%s\"...", inputFileName, outputFileName);
-//
-//    // Loop through file and save records to struct until end-of-file.
-//    int recordCount = 0;
-//    while (!feof(inputCSVFilePtr)) {
-//
-//        // Scan data; save it to appropriate variables in struct.
-//        // For strings, skip whitespaces and commas.
-//        fscanf(inputCSVFilePtr, " %d , %19[^,] , %19[^,] , %lf , %lf \n",
-//               &tempCustomer.AccountNumber,
-//               tempCustomer.FirstName,
-//               tempCustomer.LastName,
-//               &tempCustomer.AccountBalance,
-//               &tempCustomer.LastPaymentAmount);
-//
-//        // Write struct to in binary to DAT file.
-//        fwrite(&tempCustomer, sizeof(CustomerData), 1, outputDATFilePtr);
-//
-//        ++recordCount;
-//    }
-//    fclose(inputCSVFilePtr);
-//    fclose(outputDATFilePtr);
-//
-//    printf("\n%d record(s) were successfully written to \"%s\"\n", recordCount, outputFileName);
-//}
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_CHAR_LENGTH  20
+#define INPUT_FILE_NAME  "accounts.csv"
+#define OUTPUT_FILE_NAME "accounts.dat"
+
+typedef struct {
+    int    AccountNumber;
+    char   FirstName[MAX_CHAR_LENGTH];
+    char   LastName[MAX_CHAR_LENGTH];
+    double AccountBalance;
+    double LastPaymentAmount;
+} CustomerData;
+
+
+// DRIVES THE PROGRAM
+int main() {
+
+    FILE *inputCSVFilePtr;  // Pointer to CSV file to be read.
+    FILE *outputDATFilePtr; // Pointer to DAT file to be created.
+
+    // (1) Open CSV input file in read-only mode.
+    if ((inputCSVFilePtr = fopen(INPUT_FILE_NAME, "rb")) == NULL) {
+        printf("\nERROR: \"%s\" not found. Terminating operation.", INPUT_FILE_NAME);
+        exit(EXIT_FAILURE);
+    }
+
+    // (2) Check if DAT output file already exists.
+    if ((outputDATFilePtr = fopen(OUTPUT_FILE_NAME, "rb")) != NULL) {
+
+        // DAT file confirmed to exist and is currently opened in read-only mode.
+        // Immediately close file; if user wants to overwrite current file, it will be re-opened in write mode.
+        fclose(outputDATFilePtr);
+
+        // (3) If DAT file already exists, ask user if they want to overwrite it.
+        char userChoice;
+        printf("\n"
+               "File \"%s\" already exists. Overwrite?\n"
+               "y or n: ", OUTPUT_FILE_NAME);
+        scanf("%c", &userChoice);
+
+        while (userChoice != 'y' && userChoice != 'n') {
+            puts("Please enter 'y' for yes or 'n' for no.\n");
+            scanf("%c", &userChoice);
+        }
+
+        if (userChoice == 'n') {
+            printf("\n"
+                   "File \"%s\" will not be overwritten. Terminating program.\n", OUTPUT_FILE_NAME);
+            exit(EXIT_SUCCESS);
+        }
+    }
+
+    // (4) Open DAT file in write mode.
+    if ((outputDATFilePtr = fopen(OUTPUT_FILE_NAME, "wb")) == NULL) {
+        printf("\nERROR: \"%s\" not accessed. Terminating operation.", OUTPUT_FILE_NAME);
+        exit(EXIT_FAILURE);
+    }
+
+    // (5) Write contents from CSV file into DAT file (binary).
+    CustomerData tempCustomer = {0, "", "", 0.0, 0.0}; // Dummy data
+
+    printf("\nWriting data from \"%s\" to \"%s\"...", INPUT_FILE_NAME, OUTPUT_FILE_NAME);
+
+    // Loop through file and save records to struct until end-of-file.
+    int recordCount = 0;
+    while (!feof(inputCSVFilePtr)) {
+
+        // Scan data; save it to appropriate variables in struct.
+        // For strings, skip whitespaces and commas.
+        fscanf(inputCSVFilePtr, " %d , %19[^,] , %19[^,] , %lf , %lf \n",
+               &tempCustomer.AccountNumber,
+               tempCustomer.FirstName,
+               tempCustomer.LastName,
+               &tempCustomer.AccountBalance,
+               &tempCustomer.LastPaymentAmount);
+
+        // Write struct to in binary to DAT file.
+        fwrite(&tempCustomer, sizeof(CustomerData), 1, outputDATFilePtr);
+
+        ++recordCount;
+    }
+    fclose(inputCSVFilePtr);
+    fclose(outputDATFilePtr);
+
+    printf("\n%d record(s) were successfully written to \"%s\"\n", recordCount, OUTPUT_FILE_NAME);
+}
--- index.c
+++ index.c
@@ -1,181 +1,176 @@
-////
-//// Created by Rhys Mahannah on 2023-03-23.
-////
-//#include <stdio.h>
-//#include <string.h>
-//#include <stdlib.h>
 //
-//#define APP_NAME "Assignment08"
-//#define DAT_FILE "accounts.dat"
-//#define IDX_NAME "accounts.idx"
+// Created by Rhys Mahannah on 2023-03-23.
 //
-//#define MAX_CHAR_LENGTH 20
-//
-//// How we'll sort data. Created enum, because we may want to add more sorting fields.
-//typedef enum {
-//    ACCOUNT_BALANCE
-//} IndexKey;
-//
-//// Union not needed for one sorting option, but if we want to sort by different data types,
-//// in the future, which is a reasonable assumption, it'd be efficient to share memory space.
-//typedef union {
-//    double AccountBalance;
-//} KeyType;
-//
-//typedef struct {
-//    int AccountNumber;
-//    char FirstName[MAX_CHAR_LENGTH];
-//    char LastName[MAX_CHAR_LENGTH];
-//    double AccountBalance;
-//    double LastPaymentAmount;
-//} Customer;
-//
-//typedef struct {       // The records themselves
-//    KeyType Key;       // IndexKey for records will be AccountBalance, as per assignment instructions.
-//    long FilePosition; // The position of the record within the records file.
-//} IndexRecord;
-//
-//typedef struct {                   // Index file header; contains the metadata for records.
-//    IndexKey IndexKey;             // The field by which the data is sorted.
-//    char AppName[MAX_CHAR_LENGTH]; // The name of the index file.
-//    int RecordCount;               // The number of records in the index.
-//} IndexHeader;
-//
-//
-//// FUNCTION PROTOTYPES
-//long fileSize(FILE *input);
-//
-//int compare(const void *left, const void *right);
-//
-//
-//IndexKey indexKey;
-//
-//// DRIVES THE PROGRAM
-//int main() {
-//
-//    FILE *dataFilePtr;                // Will point to the .dat file from which we'll be reading.
-//    FILE *indexAccountBalanceFilePtr; // Will point to the .idx file we'll be creating.
-//
-//    int readCount;
-//    int writeCount;
-//    int dataRecordCount;
-//    int indexRecordCount;
-//    long filePosition;
-//
-//    IndexHeader indexHeader;
-//    IndexRecord *indexRecords;
-//
-//    indexKey = ACCOUNT_BALANCE;
-//
-//    // (1) Open the .dat binary file in read-only mode.
-//    // Check that it exists; if not, print error message and terminate program, else continue.
-//    if ((dataFilePtr = fopen(DAT_FILE, "rb")) == NULL) {
-//        printf("\nERROR: File \"%s\" not found. Terminating program.\n", DAT_FILE);
-//        exit(EXIT_FAILURE);
-//    }
-//
-//    // (2) Create an index file to which metadata will be written.
-//    // Check that it can be created; if not, print error message and terminate program, else continue.
-//    if ((indexAccountBalanceFilePtr = fopen(IDX_NAME, "wb")) == NULL) {
-//        printf("\nERROR: File \"%s\" could not be created. Terminating program.", IDX_NAME);
-//        exit(EXIT_FAILURE);
-//    }
-//
-//    // If (1) and (2) successful, start processing data.
-//    dataRecordCount = fileSize(dataFilePtr) / sizeof(Customer); // Determines number of records in .dat file.
-//
-//    // Initialize the number of indexRecords equal to the number of records we'll be reading (calculated above).
-//    indexRecords = (IndexRecord *) calloc(dataRecordCount, sizeof(IndexRecord));
-//
-//    // (3) Add metadata to index header.
-//    strcpy(indexHeader.AppName, APP_NAME);     // Cannot assign string; must copy string to destination.
-//    indexHeader.IndexKey = indexKey;
-//    indexHeader.RecordCount = dataRecordCount; // Number of records the file will contain.
-//
-//    // (4) Start writing data to index file.
-//    rewind(dataFilePtr);               // Ensure, for reading, that we're at the beginning of the .dat file.
-//    filePosition = ftell(dataFilePtr); // Determine the current position within the .dat file.
-//    indexRecordCount = 0;              // Initialize record count to 0;
-//
-//    // Read first data record; this will help confirm, below, that we're successfully reading data.
-//    // Reading ONE piece of data, the size of Customer, and reading that INTO our dummy data, which we
-//    // can then write to our .idx file.
-//    Customer tempCustomer = {0, "", "", 0.0, 0.0}; // Dummy data to which we'll be reading data from .dat file.
-//
-//    readCount = fread(&tempCustomer, sizeof(Customer), 1, dataFilePtr);
-//
-//    // Set loop that runs if not at EOF and first record, above, was read successfully.
-//    while (!feof(dataFilePtr) && readCount == 1) {
-//        indexRecords[indexRecordCount].Key.AccountBalance = tempCustomer.AccountBalance; // This is how records are org.
-//        indexRecords[indexRecordCount].FilePosition = filePosition;                      // Position of record in file.
-//
-//        ++indexRecordCount;                // Move to the next record to be written.
-//        filePosition = ftell(dataFilePtr); // Save the position where next record will be written.
-//
-//        readCount = fread(&tempCustomer, sizeof(Customer), 1, dataFilePtr); // Load next file to be written.
-//    }
-//
-//    // (5) Organize data in header according to AccountBalance descending, as per assignment instructions.
-//    // qsort() takes as its fourth argument a function pointer, so we must pass in the ADDRESS of the compare function.
-//    qsort(indexRecords, indexRecordCount, sizeof(IndexRecord), &compare);
-//
-//    printf("Index Header Size: %lu\n"
-//           "Index Record Size: %lu\n"
-//           "\n",
-//           sizeof(IndexHeader),
-//           sizeof(IndexRecord));
-//
-//    puts("Creating index file...\n");
-//
-//    // Write Index Header to file -- this contains the metadata for the records it contains.
-//    fwrite(&indexHeader, sizeof(IndexHeader), 1, indexAccountBalanceFilePtr);
-//
-//    // Write records to file -- this is the actual data being stored.
-//    writeCount = fwrite(indexRecords, sizeof(IndexRecord), indexRecordCount, indexAccountBalanceFilePtr);
-//
-//    printf("%d records were processed.\n", writeCount);
-//    printf("Index file size: %ld\n", fileSize(indexAccountBalanceFilePtr));
-//
-//    // Free memory dynamically allocated to create records.
-//    free(indexRecords);
-//
-//    // Close both files.
-//    fclose(dataFilePtr);
-//    fclose(indexAccountBalanceFilePtr);
-//}
-//
-//
-//// FUNCTION DEFINITIONS
-//
-//// Determines the size of the file based on the size of one record and the total number of records.
-//long fileSize(FILE *input) {
-//    long orgPos;
-//    long startPos;
-//    long endPos;
-//
-//    orgPos = ftell(input); // Save original file position
-//    rewind(input);
-//
-//    startPos = ftell(input);
-//    fseek(input, 0, SEEK_END);
-//
-//    endPos = ftell(input);
-//    fseek(input, orgPos, SEEK_SET); // Restore original file position
-//
-//    return (endPos - startPos);
-//}
-//
-//// Compares left with right; returns 1 if descending order.
-//int compare(const void *left, const void *right) {
-//    // Cast pointer to parameters to IndexRecord.
-//    IndexRecord *pLeft = (IndexRecord *) left;
-//    IndexRecord *pRight = (IndexRecord *) right;
-//
-//    // Return result based on size of Account Balance.
-//    if (pLeft->Key.AccountBalance > pRight->Key.AccountBalance) {
-//        return -1; // This would be ascending order, which we don't want.
-//    } else if (pLeft->Key.AccountBalance < pRight->Key.AccountBalance) {
-//        return 1;  // This allows us to sort Account Balance in descending order.
-//    }
-//    return 0; // If the Account Balances are the same,
-//}
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define APP_NAME "Assignment08"
+#define DAT_FILE "accounts.dat"
+#define IDX_NAME "accounts.idx"
+
+#define MAX_CHAR_LENGTH 20
+
+// How we'll sort data. Created enum, because we may want to add more sorting fields.
+typedef enum {
+    ACCOUNT_BALANCE
+} IndexKey;
+
+// Union not needed for one sorting option, but if we want to sort by different data types,
+// in the future, which is a reasonable assumption, it'd be efficient to share memory space.
+typedef union {
+    double AccountBalance;
+} KeyType;
+
+typedef struct {
+    int AccountNumber;
+    char FirstName[MAX_CHAR_LENGTH];
+    char LastName[MAX_CHAR_LENGTH];
+    double AccountBalance;
+    double LastPaymentAmount;
+} Customer;
+
+typedef struct {
+    KeyType Key;
+    long FilePosition;
+} IndexRecord;
+
+typedef struct {                   // Index file header; contains the metadata for records.
+    IndexKey IndexKey;             // The field by which the data is sorted.
+    char AppName[MAX_CHAR_LENGTH]; // The name of the index file.
+    int RecordCount;               // The number of records in the index.
+} IndexHeader;
+
+
+// FUNCTION PROTOTYPES
+long fileSize(FILE *input);
+
+int compare(const void *left, const void *right);
+
+
+IndexKey indexKey;
+
+// DRIVES THE PROGRAM
+int main() {
+
+    FILE *dataFilePtr;                // Will point to the .dat file from which we'll be reading.
+    FILE *indexAccountBalanceFilePtr; // Will point to the .idx file we'll be creating.
+
+    int readCount;
+    int writeCount;
+    int dataRecordCount;
+    int indexRecordCount;
+    long filePosition;
+
+    IndexHeader indexHeader;
+    IndexRecord *indexRecords;
+
+    indexKey = ACCOUNT_BALANCE;
+
+    // (1) Open DAT file in read-only mode.
+    if ((dataFilePtr = fopen(DAT_FILE, "rb")) == NULL) {
+        printf("\nERROR: File \"%s\" not found. Terminating program.\n", DAT_FILE);
+        exit(EXIT_FAILURE);
+    }
+
+    // (2) Create IDX file to which data will be written.
+    if ((indexAccountBalanceFilePtr = fopen(IDX_NAME, "wb")) == NULL) {
+        printf("\nERROR: File \"%s\" could not be created. Terminating program.", IDX_NAME);
+        exit(EXIT_FAILURE);
+    }
+
+    // If (1) and (2) successful, start processing data.
+    dataRecordCount = fileSize(dataFilePtr) / sizeof(Customer); // Determines number of records in .dat file.
+
+    // Initialize the number of indexRecords equal to the number of records we'll be reading (calculated above).
+    indexRecords = (IndexRecord *) calloc(dataRecordCount, sizeof(IndexRecord));
+
+    // (3) Add metadata to index header.
+    strcpy(indexHeader.AppName, APP_NAME);     // Cannot assign string; must copy string to destination.
+    indexHeader.IndexKey = indexKey;
+    indexHeader.RecordCount = dataRecordCount; // Number of records the file will contain.
+
+    // (4) Start writing data to index file.
+    rewind(dataFilePtr);               // Ensure, for reading, we're at beginning of DAT file.
+    filePosition = ftell(dataFilePtr); // Determine current position within DAT file.
+    indexRecordCount = 0;
+
+    // (5) Read first data record; this will help confirm, below, that we're successfully reading data.
+    Customer tempCustomer = {0, "", "", 0.0, 0.0};
+    readCount = fread(&tempCustomer, sizeof(Customer), 1, dataFilePtr);
+
+    // (6) Loop runs if not EOF AND if first record, above, was read successfully.
+    while (!feof(dataFilePtr) && readCount == 1) {
+        indexRecords[indexRecordCount].Key.AccountBalance = tempCustomer.AccountBalance; // This is how records are org.
+        indexRecords[indexRecordCount].FilePosition = filePosition;                      // Position of record in file.
+
+        ++indexRecordCount;                // Move to the next record to be written.
+        filePosition = ftell(dataFilePtr); // Save the position where next record will be written.
+
+        readCount = fread(&tempCustomer, sizeof(Customer), 1, dataFilePtr); // Load next file to be written.
+    }
+
+    // (&) Organize data in header according to AccountBalance descending, as per assignment instructions.
+    // qsort() takes as its fourth argument a function pointer, so we must pass ADDRESS of compare function.
+    qsort(indexRecords, indexRecordCount, sizeof(IndexRecord), &compare);
+
+    printf("Index Header Size: %lu\n"
+           "Index Record Size: %lu\n"
+           "\n",
+           sizeof(IndexHeader),
+           sizeof(IndexRecord));
+
+    puts("Creating index file...\n");
+
+    // Write Index Header to file -- this contains the metadata for the records it contains.
+    fwrite(&indexHeader, sizeof(IndexHeader), 1, indexAccountBalanceFilePtr);
+
+    // Write records to file -- this is the actual data being stored.
+    writeCount = fwrite(indexRecords, sizeof(IndexRecord), indexRecordCount, indexAccountBalanceFilePtr);
+
+    printf("%d records were processed.\n", writeCount);
+    printf("Index file size: %ld\n", fileSize(indexAccountBalanceFilePtr));
+
+    // Free memory dynamically allocated to create records.
+    free(indexRecords);
+
+    // Close both files.
+    fclose(dataFilePtr);
+    fclose(indexAccountBalanceFilePtr);
+}
+
+
+// FUNCTION DEFINITIONS
+
+// Determines the size of the file based on the size of one record and the total number of records.
+long fileSize(FILE *input) {
+    long orgPos;
+    long startPos;
+    long endPos;
+
+    orgPos = ftell(input); // Save original file position
+    rewind(input);
+
+    startPos = ftell(input);
+    fseek(input, 0, SEEK_END);
+
+    endPos = ftell(input);
+    fseek(input, orgPos, SEEK_SET); // Restore original file position
+
+    return (endPos - startPos);
+}
+
+// Compares left with right; returns 1 if descending order.
+int compare(const void *left, const void *right) {
+    // Cast pointer to parameters to IndexRecord.
+    IndexRecord *pLeft = (IndexRecord *) left;
+    IndexRecord *pRight = (IndexRecord *) right;
+
+    // Return result based on size of Account Balance.
+    if (pLeft->Key.AccountBalance > pRight->Key.AccountBalance) {
+        return -1; // This would be ascending order, which we don't want.
+    } else if (pLeft->Key.AccountBalance < pRight->Key.AccountBalance) {
+        return 1;  // This allows us to sort Account Balance in descending order.
+    }
+    return 0; // If the Account Balances are the same,
+}
